705a95fc27adb8fddf66acab25a546d5bce68f0e
Natasha Natarajan <natashanatarajan@gmail.com>
2017-08-22 09:46:14 -0600

    added portfolio wip

---
+++ natashan_portfolio/Gruntfile.js
@@ -0,0 +1,36 @@
+/*
+ After you have changed the settings under responsive_images
+ run this with one of these options:
+  "grunt" alone creates a new, completed images directory
+  "grunt clean" removes the images directory
+  "grunt responsive_images" re-processes images without removing the old ones
+*/
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          /* Change this to create different image sizes */
+          sizes: [{
+            width: 1600,
+            suffix: "_large_2x",
+            quality: 30
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.registerTask('default', ['responsive_images']);
+
+};
